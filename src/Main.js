cf7db1cd25740223255bc394d01572cbd1406230
nathias <n@th1.as>
2018-08-23 15:53:22 +0200

    blog route

--- src/Main.js
+++ src/Main.js
@@ -2,27 +2,31 @@ import React, { Component } from "react";
 import {
   Route,
   NavLink,
-  HashRouter
+  HashRouter,
+  Link,
+  Redirect,
 } from "react-router-dom";
 import Home from "./Home";
 import Id from "./Id";
 import Contact from "./Contact";
+import Blog from "./Blog";
 
 class Main extends Component {
   render() {
     return (
-    
       <HashRouter>
         <div>
           <ul className="header">
-            <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink to="/id">id</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
+            <li><NavLink exact to="/">main()</NavLink></li>
+            <li><NavLink to="/id">id()</NavLink></li>
+            <li><NavLink to="/contact">contact()</NavLink></li>
+            <li><NavLink to="/blog">blog()</NavLink></li>
           </ul>
           <div className="content">
             <Route exact path="/" component={Home}/>
             <Route path="/id" component={Id}/>
             <Route path="/contact" component={Contact}/>
+            <Route path="/blog" component={Blog}/>
           </div>
         </div>
       </HashRouter>
