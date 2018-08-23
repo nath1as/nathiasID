83af5a511b7286464e207e4ea67d7e59578e6010
nathias <n@th1.as>
2018-08-23 13:34:28 +0200

    Main.js created

---
+++ src/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+ 
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Nathias</h1>
+        <ul className="header">
+          <li><a href="/">main()</a></li>
+          <li><a href="/stuff">id()</a></li>
+          <li><a href="/contact">contact()</a></li>
+        </ul>
+        <div className="content">
+
+        </div>
+      </div>
+    );
+  }
+}
+ 
+export default Main;
