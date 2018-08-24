145840b7544cce240b1a744294fee27f09160083
nathias <n@th1.as>
2018-08-24 23:28:39 +0200

    contacts

--- src/Contact.js
+++ src/Contact.js
@@ -6,35 +6,36 @@ class Contact extends Component {
   render() {
     return (
       <div>
-        <ul>
-          <li>email: <a
-            href="mailto:n@th1.as"
-            title='email'
-            target='_blank'
-            rel='noopener noreferrer'>
-           n@th1.as
-          </a></li>
-          <li>tox: <a
-            title='tox'
-            target='_blank'
-            rel='noopener noreferrer'>
-           123219391391
-          </a></li>
-          <li>irc: nathias</li>
-          <li>keybase: nathias</li>
-        </ul>
-        <ul>
-          <li><img src={github} alt="github" />github</li>
-          <li><img />academia.edu</li>
-          <li><img />deviantart</li>
+        <article>
+          <ul>
+            <li>email: <a
+              href="mailto:n@th1.as"
+              title='email'
+              target='_blank'
+              rel='noopener noreferrer'>
+             n@th1.as
+            </a></li>
+            <li>tox: <a
+              title='tox'
+              target='_blank'
+              rel='noopener noreferrer'>
+             123219391391
+            </a></li>
+            <li>irc: nathias</li>
+            <li>keybase: nathias</li>
+          </ul>
+          <ul>
+            <li><img />github</li>
+            <li><img />academia.edu</li>
+            <li><img />deviantart</li>
 
-          <li><img />reddit</li>
-          <li><img />twitter</li>
-          <li><img />instagram</li>
-          <li><img />linkedin</li>
-          <li><img src={facebook} alt="facebook" />facebook</li>
-        </ul>
-
+            <li><img />reddit</li>
+            <li><img />twitter</li>
+            <li><img />instagram</li>
+            <li><img />linkedin</li>
+            <li><img />facebook</li>
+          </ul>
+        </article>
       </div>
     );
   }
