2a920d011879587d7d6da8ff919870421ca25409
nathias <n@th1.as>
2018-08-25 00:17:26 +0200

    submenu different glitch

--- src/Whois.js
+++ src/Whois.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
+import nathias from './files/img/nathias.png';
 
 class Whois extends Component {
   render() {
     return (
       <div>
-        <p>
-          class Nathias(Philosopher, Coder):<br/>
-              &nbsp;&nbsp;&nbsp;&nbsp; name = Matija<br/>
-              &nbsp;&nbsp;&nbsp;&nbsp; surname = Potocnik Pribosic<br/>
+        <article>
+          <img src={nathias} />
+          <p>
+            class Nathias(Philosopher, Coder):<br/>
+                &nbsp;&nbsp;&nbsp;&nbsp; name = Matija<br/>
+                &nbsp;&nbsp;&nbsp;&nbsp; surname = Potocnik Pribosic<br/>
 
 
-              &nbsp;&nbsp;&nbsp;&nbsp; education =: doctor philosophia<br/>
-              &nbsp;&nbsp;&nbsp;&nbsp; experience =: list<br/>
-        </p>
+                &nbsp;&nbsp;&nbsp;&nbsp; education =: doctor philosophia<br/>
+                &nbsp;&nbsp;&nbsp;&nbsp; experience =: list<br/>
+          </p>
+        </article>
       </div>
     );
   }
--- src/Work.js
+++ src/Work.js
@@ -4,14 +4,16 @@ class Work extends Component {
   render() {
     return (
       <div>
-        <h3> Philosophy:</h3>
-        <p>
-          cobiss
-        </p>
-        <h3> Code:</h3>
-        <p>
-          github
-        </p>
+        <article>
+          <h3> Philosophy:</h3>
+          <p>
+            cobiss
+          </p>
+          <h3> Code:</h3>
+          <p>
+            github
+          </p>
+        </article>
       </div>
     );
   }
