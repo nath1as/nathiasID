483ded52fd1c1c83875ed9cf168d8b9530655479
nathias <n@th1.as>
2018-08-24 14:21:02 +0200

    added Online.js

---
+++ src/Online.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Online extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li><img />github</li>
+          <li><img />keybase</li>
+          <li><img />deviantart</li>
+          <li><img />behance</li>
+          <li><img />bananagif</li>
+
+          <li><img />reddit</li>
+          <li><img />twitter</li>
+          <li><img />instagram</li>
+          <li><img />linkedin</li>
+          <li><img />facebook</li>
+        </ul>
+      </div>
+    );
+  }
+}
+ 
+export default Online;
