477ef75fbd4312d94250d5ebd4ebdc199f03b846
nathias <n@th1.as>
2018-08-27 15:24:45 +0200

    glitch2blink animation

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
 import "./normalize.css";
-// import "./skeleton.css";
 import "./index.css";
 
 ReactDOM.render(
