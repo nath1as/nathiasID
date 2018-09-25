abb99b9ae133917f1e8aed91b052ebd0e479494b
nathias <n@th1.as>
2018-09-25 21:06:49 +0200

    exact

--- src/Main.js
+++ src/Main.js
@@ -18,8 +18,8 @@ class Main extends Component {
         <nav className="nav">
           <ul>
             <li><NavLink exact to="/">main()</NavLink></li>
-            <li><NavLink to="/id"><span>id()</span></NavLink></li>
-            <li><NavLink to="/contact">contact()</NavLink></li>
+            <li><NavLink exact to="/id"><span>id()</span></NavLink></li>
+            <li><NavLink exact to="/contact">contact()</NavLink></li>
           </ul>
           <div>
             <Route exact path="/" component={Void}/>
