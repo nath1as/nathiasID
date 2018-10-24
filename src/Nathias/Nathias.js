fde5230a2257fa332f92d1b03e37ff06f5d0f689
nathias <n@th1.as>
2018-10-24 10:09:05 +0200

    testing resposivneness on old mobile

--- src/Nathias/Nathias.js
+++ src/Nathias/Nathias.js
@@ -2,29 +2,31 @@ import React, { Component } from "react";
 import "./Nathias.css";
 
 const nths = `
-█▄
-███▄
-███▀█▄
-███  ▀█▄              █
-███   ███             █     ▄              ▄▄▄
-███   ███     ▄█▄     █     █       ▄█▄  ▄█▀ ▀▀
-███   ███   ▄█▀ ▀█▄  ▀█▀ ▄▄▄█ ▀   ▄█▀ ▀█▄ ▀█▄▄
-███   ███ ▄██▄   ▄██▄ █▄ █  █ █ ▄██▄   ▄██▄  ▀█▄
-███   ███                █             ▄▄▄▄▄▄▄█▀
-███   ███
-███   █▀
-███
-███
-█▀
+ █▄
+ ███▄
+ ███▀█▄
+ ███  ▀█▄              █
+ ███   ███             █     ▄              ▄▄▄   
+ ███   ███     ▄█▄     █     █       ▄█▄  ▄█▀ ▀▀  
+ ███   ███   ▄█▀ ▀█▄  ▀█▀ ▄▄▄█ ▀   ▄█▀ ▀█▄ ▀█▄▄   
+ ███   ███ ▄██▄   ▄██▄ █▄ █  █ █ ▄██▄   ▄██▄  ▀█▄ 
+ ███   ███                █             ▄▄▄▄▄▄▄█▀ 
+ ███   ███
+ ███   █▀
+ ███
+ ███
+ █▀
 `;
 
 class Logo extends Component {
   render() {
     return (
       <div className="logo-wrap">
-        <pre id="logo" className="glitch">
-          { nths }
-        </pre>
+        <code className="logo-wrap">
+          <pre id="logo" className="glitch">
+            { nths }
+          </pre>
+        </code>
       </div>
     );
   }
