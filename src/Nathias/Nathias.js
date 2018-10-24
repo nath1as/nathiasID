a1fbe07e9f4435a5ff7142e9f7dfedc8dec5762a
nathias <n@th1.as>
2018-10-24 19:51:34 +0200

    whitespace width issue fix

--- src/Nathias/Nathias.js
+++ src/Nathias/Nathias.js
@@ -70,9 +70,9 @@ class Logo extends Component {
   render() {
     return (
       <div className="logo-wrap">
-        <div id="logo" className="glitch">
+        <pre id="logo" className="glitch">
           { nths }
-        </div>
+        </pre>
       </div>
     );
   }
